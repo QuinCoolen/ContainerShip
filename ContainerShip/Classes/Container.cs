567afb087b5cf0279f2e0345fd27a5ac0581e90b
Quin Coolen <quin.coolen@gmail.com>
2025-01-14 13:24:00 +0100

    Add container and container types

---
+++ ContainerShip/Classes/Container.cs
@@ -0,0 +1,53 @@
+namespace ContainerShip.Classes;
+
+public abstract class Container
+{
+    public int Weight { get; set; } = 4;
+    public static int MaxWeight { get; set; } = 30;
+    public bool IsValuable { get; set; }
+    public bool RequiresCooling { get; set; }
+    public ContainerType Type { get; set; } = ContainerType.Regular;
+
+    public Container(ContainerType type, int weight)
+    {
+
+        if (weight > (MaxWeight - Weight))
+        {
+            throw new Exception("Container is too heavy");
+        }
+
+        Weight += weight;
+
+        IsValuable = type is ContainerType.Valuable;
+        RequiresCooling = type is ContainerType.Coolable;
+        Type = type;
+    }
+}
+
+public class RegularContainer : Container
+{
+    public RegularContainer(int weight) : base(ContainerType.Regular, weight)
+    {
+        IsValuable = false;
+        RequiresCooling = false;
+    }
+}
+
+public class CoolableContainer : Container
+{
+    public CoolableContainer(int weight) : base(ContainerType.Coolable, weight)
+    {
+        RequiresCooling = true;
+        Type = ContainerType.Coolable;
+    }
+}
+
+public class ValuableContainer : Container
+{
+
+    public ValuableContainer(int weight) : base(ContainerType.Valuable, weight)
+    {
+        IsValuable = true;
+        Type = ContainerType.Valuable;
+    }
+}
---
+++ ContainerShip/Classes/ContainerType.cs
@@ -0,0 +1,8 @@
+namespace ContainerShip.Classes;
+
+public enum ContainerType
+{
+    Regular = 1,
+    Valuable = 2,
+    Coolable = 3,
+}
